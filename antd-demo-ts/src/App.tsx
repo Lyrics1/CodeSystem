ed36ec497d18df50a0ecde98e81803c35a97ad69
lyricszhang <lyricszhang@tencent.com>
2018-11-23 17:57:40 +0800

    初始化项目

--- antd-demo-ts/src/App.tsx
+++ antd-demo-ts/src/App.tsx
@@ -1,20 +1,40 @@
 import * as React from 'react';
 import './App.css';
+import { Layout, Menu, Breadcrumb, } from 'antd';
+const { Header, Content, } = Layout;
 
-import logo from './logo.svg';
-
+import { Slide } from './controller/slide'
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <Layout>
+        <Header className="header">
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Layout>
+          <Slide />
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: '80vh' }}>
+              Content
+          </Content>
+          </Layout>
+        </Layout>
+      </Layout>
     );
   }
 }
